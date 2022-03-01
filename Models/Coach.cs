eac48671fe1d56b562e930a279bed5629ff26ed3
avgolberg <av.golberg@gmail.com>
2022-03-01 12:09:52 +0200

    Добавьте файлы проекта.

---
+++ Models/Coach.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sports_Coaches.Models
+{
+    class Coach
+    {
+        public int Id { get; set; }
+
+        [Required, Column("Full name")]
+        public string FullName { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime DateOfBirth { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+        public string PhotoUrl { get; set; }
+
+
+        //public Person Person { get; set; }
+        //public string Holiday { get; set; }
+
+        
+
+        //public Category Category { get; set; } 
+
+        //public Status Status { get; set; }
+        //public string WhereToBuy { get; set; }
+
+        //public double ApproximatePrice { get; set; }
+        //public bool GiftWrapping { get; set; }
+
+        //public string Comment { get; set; }
+    }
+}
